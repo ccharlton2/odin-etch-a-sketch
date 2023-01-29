04a41a4df37ec3ab76400fc1ed7520c339e9ebef
Christian <christiancharlton17@gmail.com>
2023-01-28 17:11:38 -0700

    Get grid value from user

--- javasctipt.js
+++ javasctipt.js
@@ -5,10 +5,34 @@
 //     e.target.style.backgroundColor = 'black';
 // })
 
-let gridValue = 16;
 const mainContainer = document.querySelector('.container');
+const drawButton = document.querySelector('.draw');
+const gridDimensionInput = document.querySelector('.grid-value');
+
+drawButton.addEventListener('click', () => {
+    let gridDimensionInput = document.querySelector('.grid-value');
+    let gridValue = getGridDimension(gridDimensionInput.value);
+    drawGrid(gridValue);
+});
+
+function getGridDimension(input) {
+// validate is number, trim, if blank space default 16
+    if (input.trim() === '') {
+        return 16;
+    }
+    else {
+        let trimmedInput = input.trim();
+        if (parseInt(trimmedInput)) {
+            return trimmedInput;
+        }
+        else {
+            return 'No characters please!'
+        }
+    }
+}
 
 function drawGrid(gridValue) {
+    mainContainer.textContent = '';
     // for loop 1
     for (let rowIndex = 0; rowIndex < gridValue; rowIndex++) {
         let gridRow = document.createElement('div');
